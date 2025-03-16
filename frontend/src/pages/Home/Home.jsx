712713bcf58f0f44a05d940a96ea5248228c81e3
Dulitha Perera <dulitha.22@cse.mrt.ac.lk>
2025-03-16 10:40:16 +0530

    Create explore menu frontend part

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.jsx'
 import Header from '../../components/Header/Header.jsx'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu.jsx'
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <ExploreMenu/>
     </div>
   )
 }
