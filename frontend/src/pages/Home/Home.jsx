a25d02850d0aa4f1bb618e4a2133cfb08a620826
Dulitha Perera <dulitha.22@cse.mrt.ac.lk>
2025-03-16 09:56:10 +0530

    Create home page header

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Home.jsx'
+import Header from '../../components/Header/Header.jsx'
 
 const Home = () => {
   return (
     <div>
-      
+      <Header/>
     </div>
   )
 }
