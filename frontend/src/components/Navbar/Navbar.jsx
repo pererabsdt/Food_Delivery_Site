b18f3c6e03e6dbdcd68d25b46ed88fc70033b4b5
Dulitha Perera <dulitha.22@cse.mrt.ac.lk>
2025-04-09 17:45:54 +0530

    Create cart page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = ({setShowLogin}) => {
 
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt="logo" className="logo"/>
+      <Link to='/'><img src={assets.logo} alt="logo" className="logo"/></Link>
       <ul className='navbar-menu'>
         <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</Link>
         <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</a>
@@ -20,7 +20,7 @@ const Navbar = ({setShowLogin}) => {
       <div className='navbar-right'>
         <img src={assets.search_icon} alt="search icon"/>
         <div className="navbar-search-icon">
-            <img src={assets.basket_icon} alt="basket icon"/>
+            <Link to='/cart'><img src={assets.basket_icon} alt="basket icon"/></Link>
             <div className='dot'></div>
         </div>
         <button onClick={()=>setShowLogin(true)}>Sign in</button>
