291203f6e73bcd0cc12b6cb72c73518ae216c6f6
Dulitha Perera <dulitha.22@cse.mrt.ac.lk>
2025-04-11 22:58:48 +0530

    Create functionality to display all food items in the admin panel

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -3,9 +3,7 @@ import './List.css'
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-const List = () => {
-
-  const url = "http://localhost:4000"
+const List = ({url}) => {
 
   const [list, setList] = useState([]);
 
@@ -18,13 +16,43 @@ const List = () => {
     }
   }
 
+  const removeFood = async(foodId) => {
+    const response = await axios.post(`${url}/api/remove`, {id:foodId});
+    await fetchList();
+    if (response.data.success) {
+      toast.success(response.data.message)
+    } else {
+      toast.error(response.data.message)
+    }
+  }
+
   useEffect(()=>{
     fetchList();
   },[])
 
   return (
-    <div>
-      
+    <div className='list add flex-col'>
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item, index)=>{
+          return (
+            <div key={index} className='list-table-format'>
+              <img src={`${url}/images/`+item.image} alt="foot item" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>${item.price}</p>
+              <p onClick={()=>removeFood(item._id)} className='cross'>✕</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
