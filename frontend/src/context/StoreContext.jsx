06e891b455011ab0dec1d068753ab087e6aa831e
Dulitha Perera <dulitha.22@cse.mrt.ac.lk>
2025-04-12 07:20:57 +0530

    Integrate client and server for login

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,6 +7,9 @@ const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
 
+    const url = 'http://localhost:4000';
+    const [token, setToken] = useState("");
+
     const addToCart = (itemId) => {
         if (!cartItems[itemId]) {
             setCartItems((prev)=>({...prev,[itemId]:1}))
@@ -36,7 +39,10 @@ const StoreContextProvider = (props) => {
         setCartItems,
         addToCart,
         removeFromCart,
-        getTotalCartAmount
+        getTotalCartAmount,
+        url,
+        token,
+        setToken
     }
 
     return (
