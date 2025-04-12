558e7f5bc731df03574f9bd1dacab155c9289c92
Dulitha Perera <dulitha.22@cse.mrt.ac.lk>
2025-04-12 10:47:21 +0530

    Create server side to get cart

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -2,39 +2,47 @@ import userModel from "../models/userModel.js"
 
 const addToCart = async (req, res) => {
     try {
-        let userData = await userModel.findById(req.body.userId);
+        let userData = await userModel.findById(req.userId);
         let cartData = await userData.cartData;
         if (!cartData[req.body.itemId]) {
             cartData[req.body.itemId] = 1;
         } else {
             cartData[req.body.itemId] += 1;
         }
-        await userModel.findByIdAndUpdate(req.body.userId, {cartData});
+        await userModel.findByIdAndUpdate(req.userId, {cartData});
         res.json({success:true, message:"Added to cart"})
     } catch (error) {
         console.log(error);
-        res.json({success:false, message:error})
+        res.json({success:false, message:error.message})
     }
 }
 
 
 const removeFromCart = async (req, res) => {
     try {
-        let userData = await userModel.findById(req.body.userId);
+        let userData = await userModel.findById(req.userId);
         let cartData = await userData.cartData;
-        if (cartData[req.body.itemId] > 0) {
+        if (cartData[req.body.itemId] > 0){
             cartData[req.body.itemId] -=1;
         }
-        await userModel.findByIdAndUpdate(req.body.userId, {cartData});
+        await userModel.findByIdAndUpdate(req.userId, {cartData});
         res.json({success:true, message:"Removed from cart"})
     } catch (error) {
         console.log(error);
-        res.json({success:false, message:error})
+        res.json({success:false, message:error.message})
     }
 }
 
 
 const getCart = async (req, res) => {
+    try {
+        let userData = await userModel.findById(req.userId);
+        let cartData = await userData.cartData;
+        res.json({success:true, cartData})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message})
+    }
 
 }
 
